e424fb47821f679ba774f61b9130b1ecca6756a2
rocke97 <aarongunther97@gmail.com>
2018-04-14 18:21:23 -0500

    Added steal_gold action and some helper functions

--- games/pirates/steal_gold.hpp
+++ games/pirates/steal_gold.hpp
@@ -2,7 +2,22 @@
 #define __STEAL_GOLD__
 
 #include "ai.hpp"
+#include "action.hpp"
 
 using namespace cpp_client::pirates;
 
+class steal_gold : public action
+{
+    public:
+    steal_gold(AI* aip, Unit u) : action(aip, u)
+    {
+        name = "steal_gold";
+    }
+    
+    bool can_execute();
+
+    bool execute();
+
+    void move_towards();
+};
 #endif
