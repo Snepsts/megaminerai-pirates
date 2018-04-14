fa62d5ff72762e8151572435b88b4e8867ed922c
rocke97 <aarongunther97@gmail.com>
2018-04-14 15:29:09 -0500

    Added filter_action_by_job

--- games/pirates/turn.cpp
+++ games/pirates/turn.cpp
@@ -1,8 +1,15 @@
 #include "turn.hpp"
- 
+extern std::vector<Action> ACTION_VECTOR;
 
+using namespace cpp_client::pirates;
 std::vector<Action> filter_action_by_job(Unit u)
 {
-    GAMES_PIRATES_PLAYER_H
-    for(std::pair<std::string, Action> kv : )
+    std::vector<Action> filtered;
+    for(Action entry : ACTION_VECTOR)
+    {
+        if() //decide how to filter
+        {
+            filtered.push_back(entry);
+        }
+    }
 }
