671b1665c828e8f7a586449425438aebf515a03d
Michael Ranciglio <rancigliom@gmail.com>
2018-04-15 00:26:39 -0500

    Add spawning logic

--- games/pirates/ai.cpp
+++ games/pirates/ai.cpp
@@ -625,6 +625,10 @@ void AI::spawn_units()
         else
             this->player->port->spawn("ship");
     }
+
+    if (player->port->unit->ship_health > 0) {
+        this->player->port->spawn("crew");
+    }
 }
 
 std::vector<Unit> AI::get_enemy_crew()
